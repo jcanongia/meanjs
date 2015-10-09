8cc84f428bcb180f67ea7d8c6521a490e7fbb470
jcanongia <jcanongia@gmail.com>
2015-10-09 11:39:38 -0300

    Correções

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/meanjs-app',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/meanjs-dev',
 	assets: {
 		lib: {
 			css: [
